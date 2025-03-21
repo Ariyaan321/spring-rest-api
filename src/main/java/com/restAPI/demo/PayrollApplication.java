1ec96cd463d46b1660a99a4a58c29bba43a64d56
Ariyaan <134138177+Ariyaan321@users.noreply.github.com>
2025-03-21 22:00:05 +0530

    create spring

---
+++ src/main/java/com/restAPI/demo/PayrollApplication.java
@@ -0,0 +1,13 @@
+package com.restAPI.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayrollApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayrollApplication.class, args);
+	}
+
+}
